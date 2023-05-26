5ee7e89294798ea27846bb37cbd423e3cd4f8f75
elmaccho <maciek.chojnacki22@wp.pl>
2023-05-26 20:15:36 +0200

    Update StartingLogo.js

--- components/StartingLogo.js
+++ components/StartingLogo.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const StartingLogo = () => {
     return(
         <View>
-            <Text>FreeMe</Text>
+            <Image
+                source={require('../assets/icon.png')}
+                style={styles.icon}
+            />
+            <Text
+                style={styles.text}
+            >
+                FreeMe
+            </Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    icon: {
+        width: 80,
+        height: 80
+    },
+    text: {
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: 600
+    }
 })
 
 export default StartingLogo
