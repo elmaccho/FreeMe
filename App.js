734f5ffc6c6ace12144d93387304837feda3501f
elmaccho <maciek.chojnacki22@wp.pl>
2023-05-26 18:26:15 +0200

    icon

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartingLogo from './components/StartingLogo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.logo}>
+        <StartingLogo/>
+      </View>
     </View>
   );
 }
---
+++ components/StartingLogo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const StartingLogo = () => {
+    return(
+        <View>
+            <Text>FreeMe</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default StartingLogo
